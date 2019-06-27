e2052a1a680317fa31223f90263ba5f188001b84
yorkyu <yorkyu@tencent.com>
2019-06-27 08:09:37 +0800

    feat(router) add router

---
+++ web/router/path-to-regexp/index.js
@@ -0,0 +1,28 @@
+const pathToRegexp = require('path-to-regexp');
+
+const rule = [
+    {
+        path: '/foo/:bar',
+    },
+    {
+        path: '/foo/:bar/:name',
+    },
+    {
+        path: '/foo/:bar/?name=test',
+    },
+    {
+        path: '/foo/:bar?',
+    },
+    {
+        path: '/icon-:foo(\\d+).png'
+    }
+];
+
+rule.forEach(v => {
+    const keys = []
+    const regexp = pathToRegexp(v.path, keys);
+    console.log(v.path);
+    console.log(regexp);
+    console.log(keys);
+    console.log('\n\n');
+});
